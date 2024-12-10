6bd9fc9ed3d5f1d46d6f5b7ba1c0239d4478c9cb
Prathit-tech <ppjoshi2100@gmail.com>
2024-12-10 11:28:47 +0530

    feat:composio examples fixed

--- third_party/Composio/gmail-agent-js/trigger-example.js
+++ third_party/Composio/gmail-agent-js/trigger-example.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { AgentExecutor, createToolCallingAgent } from "langchain/agents";
-import { LangchainToolSet } from "composio-core";
+import { LangchainToolSet, Composio } from "composio-core";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatMistralAI } from "@langchain/mistralai";
 
@@ -8,36 +8,40 @@ dotenv.config();
 
 // Initialize the toolset
 const toolset = new LangchainToolSet({ apiKey: process.env.COMPOSIO_API_KEY });
+const trigger_toolset = new Composio();
 
 // Get the user
 const userEntity = await toolset.client.getEntity("default");
 
-// Set up the trigger for new emails
-await userEntity.setupTrigger("gmail", "gmail_new_gmail_message", {
+// // Set up the trigger for new emails
+await userEntity.setupTrigger("gmail", "GMAIL_NEW_GMAIL_MESSAGE", {
   interval: 10,
+  userId: "me",
+  labelIds: ["INBOX"]
 });
 
+
 // Subscribe to the trigger and listen for new emails
-toolset.client.triggers.subscribe(async (data) => {
+await trigger_toolset.triggers.subscribe( 
+  async(data) => {
   try {
-    console.log("data received", data);
-    const from = data.payload.sender;
-    const message = data.payload.messageText;
-    const id = data.payload.threadId;
-    executeAgent("default", { from, message, id });
+    await console.log("data received", data);
+    const from = await data.payload.sender;
+    const message = await data.payload.messageText;
+    const id = await data.payload.threadId;
+    const res = await executeAgent("default", { from, message, id });
   } catch (error) {
     console.log("Error: ", error);
   }
-});
+}, {triggerName: "GMAIL_NEW_GMAIL_MESSAGE"});
 // Execute when new emails are received
 async function executeAgent(entityName, { from, message, id }) {
-  try {
     // Step 1: Get the entity from the toolset
     const entity = await toolset.client.getEntity(entityName);
 
     // Step 2: Get the action for replying to a new email
     const tools = await toolset.getTools(
-      { actions: ["gmail_reply_to_thread"] },
+      { actions: ["GMAIL_REPLY_TO_THREAD"] },
       entity.id
     );
 
@@ -45,7 +49,7 @@ async function executeAgent(entityName, { from, message, id }) {
     const prompt = ChatPromptTemplate.fromMessages([
       [
         "system",
-        "You are a helpful and thorough AI email assistant who can reply to mails. Your goal is to understand the guidelines provided by the user and perform the specific actions requested by the user.",
+        "You are an AI email assistant called Mistral that can write and reply to emails. You also have access to Gmail Tools which let you execute actions on Gmail. Use the tool when the user asks you to.",
       ],
       ["human", "{input}"],
       ["placeholder", "{agent_scratchpad}"],
@@ -58,7 +62,9 @@ async function executeAgent(entityName, { from, message, id }) {
     });
 
     // Step 5: Prepare the input data
-    const body = `This is the mail you have to respond to: ${message}. It's from ${from} and the threadId is ${id}`;
+    const body = `This is the mail you have to respond to: ${message}. It's from ${from} and the threadId is ${id}
+    Generate the reply based on the ${message}. Write the subject based on the reply content generated. The sender email is ${from}.
+    Call the gmail tool and actions available to you. Add a subject line if there isnt any email.`;
     const agent = await createToolCallingAgent({ llm, tools: tools, prompt });
 
     // Step 6: Create an instance of the AgentExecutor
@@ -66,8 +72,6 @@ async function executeAgent(entityName, { from, message, id }) {
 
     // Step 7: Invoke the agent
     const result = await agentExecutor.invoke({ input: body });
-    console.log(result.output);
-  } catch (error) {
-    console.log("Error: ", error);
-  }
+    console.log(result);
+
 }
